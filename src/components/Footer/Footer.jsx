b86534ac1f3235ae06d63aa4be962693d46c2859
Alex Ticovschi <alexmticovschi@gmail.com>
2020-10-29 16:49:08 +0000

    building the Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,77 @@
 import React from "react"
-
+import { Link } from "gatsby"
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa"
 import "./footer.scss"
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-primary">
-        <div className="grid-container">
-          <div className="footer-primary__wrapper">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, recusandae?</p>
+      <div className="grid-container">
+        <div className="footer__container">
+          <div className="footer__logo">
+            <span className="footer__logo--first">Knightsbridge</span>
+            <span className="footer__logo--second">Living</span>
           </div>
+          <ul className="footer__links">
+            <li>
+              <Link to="/buildings" className="footer__link">
+                Buildings
+              </Link>
+            </li>
+            <li>
+              <Link className="footer__link" to="/knightsbridge">
+                Knightsbridge
+              </Link>
+            </li>
+            <li>
+              <Link className="footer__link" to="/living-with-us">
+                Living With Us
+              </Link>
+            </li>
+            <li>
+              <Link className="footer__link" to="/about">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link className="footer__link" to="/contact">
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link className="footer__link" to="/privacy">
+                Privacy
+              </Link>
+            </li>
+            <li>
+              <Link className="footer__link" to="/terms-and-conditions">
+                T&C
+              </Link>
+            </li>
+          </ul>
+          <ul className="footer__social-links">
+            <li>
+              <a className="footer__social-icon" href="https://twitter.com/">
+                <FaTwitter />
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer__social-icon"
+                href="https://www.instagram.com/"
+              >
+                <FaInstagram />
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer__social-icon"
+                href="https://en-gb.facebook.com/"
+              >
+                <FaFacebookF />
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </footer>
