674f3fe833ef406a99e3db959c3add7bfcd93d35
Alex Ticovschi <alexmticovschi@gmail.com>
2020-11-25 10:57:12 +0000

    adding facebook card to SEO component

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -29,12 +29,21 @@ const SEO = ({ title, description }) => {
     <Helmet htmlAttributes={{ lang: "en" }} title={`${siteTitle} - ${title}`}>
       <meta name="description" content={description || siteDescription} />
       <meta name="image" content={image} />
-      {/* {twitter card} */}
+      {/* twitter card */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={twitterUsername} />
       <meta name="twitter:title" content={siteTitle} />
       <meta name="twitter:description" content={siteDescription} />
       <meta name="twitter:image" content={`${siteUrl}${image}`} />
+
+      {/* facebook card */}
+      <meta property="og:url" content={siteUrl} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={siteTitle} />
+      <meta property="og:description" content={siteDescription} />
+      <meta property="og:image" content={`${siteUrl}${image}`} />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="300" />
     </Helmet>
   )
 }
